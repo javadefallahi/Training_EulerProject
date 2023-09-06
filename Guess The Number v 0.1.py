3a9d46ba805281642cc55c4181c679edc1e4449f
Javadefallahi <43331634+javadefallahi@users.noreply.github.com>
2023-09-06 18:21:06 +0330

    Update Guess The Number v 0.1.py

--- Guess The Number v 0.1.py
+++ Guess The Number v 0.1.py
@@ -1,4 +1,4 @@
-#Guess number
+#Guess number Game
 
 import random
 
@@ -8,15 +8,17 @@ i = "Y"
 while i == "Y":
     a1, a2 = input("\n Enter two value As Range(Seperated by Space): ").split()
     rnd = random.randint( int(a1) , int(a2) )
-
+    attempt = 1
     while True :
         y = int(input("Enter your Guess: "))
         if rnd == y:
             print (":) :) You Are Amazing :) :) The number Was ",y)
+            print ("You achieved to the right answer by", attempt, "times guesses.")
             i = input("\n Do you Want to Reapeat the Game? (Y/N): ")
             break
         elif rnd < y:
             print("Wrong:(  Enter Lesser Number \n")
+            attempt+=1
         else:
             print("Wrong:(  Enter Higher Number \n")
-        
+            attempt+=1
